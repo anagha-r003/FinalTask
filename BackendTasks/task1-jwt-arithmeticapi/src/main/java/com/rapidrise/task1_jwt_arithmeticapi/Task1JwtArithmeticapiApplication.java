12795bea8d1ab8ab6911b8362bc6030c5459c944
Anagha R nair <anagharameshannair@gmail.com>
2026-03-05 09:35:49 +0530

    Task1 base Spring Boot project setup

---
+++ BackendTasks/task1-jwt-arithmeticapi/src/main/java/com/rapidrise/task1_jwt_arithmeticapi/Task1JwtArithmeticapiApplication.java
@@ -0,0 +1,13 @@
+package com.rapidrise.task1_jwt_arithmeticapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task1JwtArithmeticapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task1JwtArithmeticapiApplication.class, args);
+	}
+
+}
---
+++ BackendTasks/task1-jwt-arithmeticapi/src/test/java/com/rapidrise/task1_jwt_arithmeticapi/Task1JwtArithmeticapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rapidrise.task1_jwt_arithmeticapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task1JwtArithmeticapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
